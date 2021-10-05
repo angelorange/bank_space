64b9eae2ae606a254ccf1566bfce7862045c3126
angelorange <acostalarangeira@gmail.com>
2021-10-05 17:06:39 -0300

    hotfix: add homepage asfirst

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       initialBinding: LoginBinding(),
-      home: LoginPageState()
+      home: HomePage()
     );
   }
 }
