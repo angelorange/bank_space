08e6d71c4ccb877c24dbabf00ac2062268d358a3
Igor L. de Jesus <lima.jesuscc@gmail.com>
2021-10-18 16:11:27 -0300

    feat: added wallet model

---
+++ lib/app/data/models/wallet_model.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+
+import 'package:bankinspace/app/data/models/user_model.dart';
+
+
+
+class Wallet {
+    Wallet({
+        required this.user,
+        required this.amount
+    });
+
+   User user;
+   double amount;
+
+}
+
+WalletfromJson(String s_wallet, User user) { 
+ 
+  Map<String, dynamic> wallet_json = jsonDecode(s_wallet);
+  Wallet wallet = Wallet (user:  user, amount:  wallet_json['wallet']['amount']);
+
+  return wallet;
+}
